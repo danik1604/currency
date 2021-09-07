5fb4655f5613cd84bd7e46f05d512a77a70d4efd
Daniils <danik@shedevre.com>
2021-09-07 11:36:10 +0300

    Initial commit

---
+++ src/Convert.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Currency;
+
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+
+class Convert
+{
+
+    private $apiUrl = "http://currency.dev39.shedevre.com";
+
+    public function exchange($ccyFrom, $ccyTo, $amount)
+    {
+        $client = new Client(['base_uri' => $this->apiUrl, 'timeout'  => 2.0]);
+        try {
+            $res = $client->request('POST', '/api/convert', [
+                'form_params' => [
+                    'ccyFrom' => $ccyFrom,
+                    'ccyTo' => $ccyTo,
+                    'amount' => $amount
+                ]
+            ]);
+
+            if($res->getStatusCode() == '200'){
+                $data = (array) @json_decode($res->getBody(), true);
+                if($data){
+                    return $data['converted'];
+                }
+            }
+        } catch (RequestException $e) {
+            return false;
+        }
+
+        return false;
+    }
+
+}
